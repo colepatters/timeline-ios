89a453bad7827f9e4fa4bf46dae3e8e6c7949c4f
Cole <cole.patter@gmail.com>
2025-09-12 16:34:08 -0500

    Initial Commit

---
+++ timeline/timelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  timelineApp.swift
+//  timeline
+//
+//  Created by Cole Patterson on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct timelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ timelineTests/timelineTests.swift
@@ -0,0 +1,17 @@
+//
+//  timelineTests.swift
+//  timelineTests
+//
+//  Created by Cole Patterson on 9/12/25.
+//
+
+import Testing
+@testable import timeline
+
+struct timelineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
